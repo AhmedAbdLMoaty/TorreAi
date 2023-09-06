5cf65fd28098ebe9bfb950e24c287af6d183e0b7
AhmedAbdLMoaty <63984192+AhmedAbdLMoaty@users.noreply.github.com>
2023-09-06 22:18:54 +0300

    Update main-controller.js

--- controllers/main-controller.js
+++ controllers/main-controller.js
@@ -17,75 +17,58 @@ pageTitle: 'hello there'
 };
 
 exports.getSearchHistory = (req, res) => {
+    const filePath = path.join('/tmp', 'searchHistory.json'); // Use /tmp directory
 
-if (fs.existsSync(filePath)) {
-
-fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) {
-        console.error(err);
+    if (fs.existsSync(filePath)) {
+        fs.readFile(filePath, 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+            } else {
+                const searchHistory = JSON.parse(data) || [];
+                res.render('search-history.ejs', { searchHistory, pageTitle: 'Hello there' });
+            }
+        });
     } else {
-        const searchHistory = JSON.parse(data) || [];
-
-
-        fs.readFile('searchHistory.json', 'utf8', (err, data) => {
-    if (err) {
-        console.error(err);  
-        res.status(500).send('Error reading search history data');
-        return;
-        
+        const initialData = [];
+        fs.writeFile(filePath, JSON.stringify(initialData), (err) => {
+            if (err) {
+                console.error(err);
+            } else {
+                console.log('Created /tmp/searchHistory.json');
+            }
+        });
+        res.render('search-history.ejs', { searchHistory: initialData, pageTitle: 'Hello there' });
     }
-
-    const searchHistory = JSON.parse(data) || [];
-
-    res.render('search-history.ejs', { searchHistory , pageTitle: 'Hello there' });
-});
 };
 
-    }
-);
-} else {
-
-const initialData = [];
-fs.writeFile(filePath, JSON.stringify(initialData), (err) => {
-    if (err) {
-        console.error(err);
-    } else {
-        console.log('Created searchHistory.json');
-
-        }
-    })}}
 
 
     const path = require('path');
     
     exports.getFav = (req, res) => {
-        const filePath = path.join(__dirname, 'favorites.json'); 
+        const tmpDir = process.env.TEMP || process.env.TMP || '/tmp'; // Use the appropriate environment variable for Windows
+        const filePath = path.join(tmpDir, 'favorites.json');
     
         if (fs.existsSync(filePath)) {
-
-        fs.readFile(filePath, 'utf8', (err, data) => {
+            fs.readFile(filePath, 'utf8', (err, data) => {
                 if (err) {
                     console.error(err);
                     res.status(500).send('Error reading favorites data');
                 } else {
                     const favorites = JSON.parse(data) || [];
-
                     res.render('fav.ejs', { favorites, pageTitle: 'My Favorites' });
                 }
             });
         } else {
-
-        const initialData = [];
+            const initialData = [];
             fs.writeFile(filePath, JSON.stringify(initialData), (err) => {
                 if (err) {
                     console.error(err);
                     res.status(500).send('Error creating favorites data');
                 } else {
-                    console.log('Created favorites.json');
-
-                    res.render('favorites.ejs', { favorites: [], pageTitle: 'My Favorites' });
+                    console.log(`Created ${filePath}`);
+                    res.render('fav.ejs', { favorites: initialData, pageTitle: 'My Favorites' });
                 }
             });
         }
     };
-    
